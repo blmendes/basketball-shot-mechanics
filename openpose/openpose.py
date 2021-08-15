27e3ba8ae3f004fc3a6c6fa8253b120661ff14c8
Bruno Luiz Mendes <42782620+blmendes@users.noreply.github.com>
2021-08-15 10:21:27 -0300

    Update openpose.py

--- openpose/openpose.py
+++ openpose/openpose.py
@@ -15,13 +15,15 @@ args = parser.parse_args()
 BODY_PARTS = { "Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
                "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
                "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
-               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 }
+               "LEye": 15, "REar": 16, "LEar": 17, "Background": 18 } #implement jump 
 
 POSE_PAIRS = [ ["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
                ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
                ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
                ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"],
                ["REye", "REar"], ["Nose", "LEye"], ["LEye", "LEar"] ]
+
+#implement jump function
 
 inWidth = args.width
 inHeight = args.height
@@ -51,8 +53,6 @@ while cv.waitKey(1) < 0:
         heatMap = out[0, i, :, :]
 
         # Originally, we try to find all the local maximums. To simplify a sample
-        # we just find a global one. However only a single pose at the same time
-        # could be detected this way.
         _, conf, _, point = cv.minMaxLoc(heatMap)
         x = (frameWidth * point[0]) / out.shape[3]
         y = (frameHeight * point[1]) / out.shape[2]
